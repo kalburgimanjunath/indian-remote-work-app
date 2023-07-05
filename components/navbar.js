c900be724e8b3d970a76c77ae33bf2dce7df6884
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-05 21:30:51 +0530

    3 files modified

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
 // import Link from '@next/Link';
+import { useState } from 'react';
 export default function Navbar() {
   const navitems1 = [
     // { title: 'sign in/sign up', link: './signin' },
@@ -18,10 +19,24 @@ export default function Navbar() {
     // { title: 'Contact Us', link: './contactus' },
     // { title: 'Privacy Policy', link: './privacypolicy' },
   ];
+  const navitems3 = [
+    { title: 'Sign in/Sign up', link: './signin' },
+    { title: 'Remote Community', link: './remotecommunity' },
+    { title: 'Remote Companies Hiring', link: './remotecompanies' },
+    { title: 'Post a Remote Job', link: './postjob' },
+    { title: 'Search remote jobs', link: './' },
+    { title: 'Contact Us', link: './contactus' },
+    { title: 'Privacy Policy', link: './privacypolicy' },
+  ];
+  const [menuOpen, setMenu] = useState(false);
   return (
     <div className="flex shadow-md sticky top-0 left-0 bg-red-50">
       <div>
-        <div className="p-2">
+        <div
+          className="pt-3 p-2"
+          onMouseOver={() => setMenu(true)}
+          onMouseOut={() => setMenu(false)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -36,6 +51,19 @@ export default function Navbar() {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
             />
           </svg>
+          <div
+            className="menu-items absolute left-2 top-10 shadow-lg border-2 bg-white-100 z-index-1 rounded-lg"
+            style={{ display: menuOpen ? 'block' : 'none' }}
+          >
+            {navitems3 &&
+              navitems3.map((item) => {
+                return (
+                  <div className="bg-white-50 hover:bg-white-100 pl-3 pr-3 p-2 hover:bg-gray-100">
+                    <a href={item.link}>{item.title}</a>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
       <div className="flex justify-between w-full">
--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,30 @@
 export default function Signin() {
   return (
-    <div className="font-bold">
-      <h1>Login To Your Account</h1>
+    <div className="">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-start-2 col-span-1 m-20 justify-items-center p-5 bg-red-300 shadow-lg hover:text-pink-100">
+          <h1 className="font-bold text-lg">Login To Your Account</h1>
+          <div>
+            <div>Your Email:</div>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+          <div>
+            <div>Your Password:</div>
+            <div>
+              <input type="text" />
+            </div>
+          </div>
+          <div>
+            <div>Forgot Your Password?</div>
+          </div>
+          <div className="justify-items-center">
+            <button className="primary">Login</button>
+            <button className="primary ml-5">Create Account</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
