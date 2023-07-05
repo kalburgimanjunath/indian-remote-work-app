ecdf98c53f533857ee7187066b48499d03343332
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-05 20:49:33 +0530

    2 files modified

--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,25 @@
 // import Link from '@next/Link';
 export default function Navbar() {
-  const navitems = [
-    { title: 'sign in/sign up', link: './signin' },
+  const navitems1 = [
+    // { title: 'sign in/sign up', link: './signin' },
     { title: 'Remote Community', link: './remotecommunity' },
     { title: 'Remote Companies Hiring', link: './remotecompanies' },
+    // { title: 'Post a Remote Job', link: './postjob' },
+    // { title: 'Search remote jobs', link: './' },
+    // { title: 'Contact Us', link: './contactus' },
+    // { title: 'Privacy Policy', link: './privacypolicy' },
+  ];
+  const navitems2 = [
+    { title: 'Sign in/Sign up', link: './signin' },
+    // { title: 'Remote Community', link: './remotecommunity' },
+    // { title: 'Remote Companies Hiring', link: './remotecompanies' },
     { title: 'Post a Remote Job', link: './postjob' },
-    { title: 'Search remote jobs', link: './' },
-    { title: 'Contact Us', link: './contactus' },
-    { title: 'Privacy Policy', link: './privacypolicy' },
+    // { title: 'Search remote jobs', link: './' },
+    // { title: 'Contact Us', link: './contactus' },
+    // { title: 'Privacy Policy', link: './privacypolicy' },
   ];
   return (
-    <div className="flex shadow-lg sticky top-0 left-0">
+    <div className="flex shadow-md sticky top-0 left-0 bg-red-50">
       <div>
         <div className="p-2">
           <svg
@@ -29,14 +38,28 @@ export default function Navbar() {
           </svg>
         </div>
       </div>
-      {navitems &&
-        navitems.map((item) => {
-          return (
-            <div className="bg-red-50 hover:bg-red-100 p-2">
-              <a href={item.link}>{item.title}</a>
-            </div>
-          );
-        })}
+      <div className="flex justify-between w-full">
+        <div className="flex">
+          {navitems1 &&
+            navitems1.map((item) => {
+              return (
+                <div className="bg-red-50 hover:bg-red-100 p-4">
+                  <a href={item.link}>{item.title}</a>
+                </div>
+              );
+            })}
+        </div>
+        <div className="flex">
+          {navitems2 &&
+            navitems2.map((item) => {
+              return (
+                <div className="bg-red-50 hover:bg-red-100 p-4">
+                  <a href={item.link}>{item.title}</a>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,3 +1,7 @@
 export default function Signin() {
-  return <div className="font-bold">Signin</div>;
+  return (
+    <div className="font-bold">
+      <h1>Login To Your Account</h1>
+    </div>
+  );
 }
