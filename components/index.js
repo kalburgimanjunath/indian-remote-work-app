c2870601fb4402dd9c33e54623f34f7f6ac07e6d
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-07-06 21:20:31 +0530

    2 files modified, 1 added

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Navbar from './navbar';
 import CompanyList from './companylist';
+import JobList from './joblist';
 import JoinUs from './JoinUs';
 import Footer from './Footer';
-export { Navbar, CompanyList, JoinUs, Footer };
+export { Navbar, CompanyList, JobList, JoinUs, Footer };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import JoinUs from '../components/JoinUs';
+import { JoinUs, JobList } from '../components/';
 import styles from '../styles/Home.module.css';
-
+import { REMOTECOMPANIES } from './remotecompanies/remotecompanies';
 export default function Home() {
   return (
     <div className="container m-20">
@@ -12,13 +12,32 @@ export default function Home() {
         that are looking for the best talented digital workers.
       </h3>
       <JoinUs />
-      <div className="flex justify-center w-full">
-        <div>Search</div>
+      <div className="flex justify-center w-full m-4 text-center align-middle">
+        <div className="m-2 flex">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </span>
+        </div>
         <div>
-          <input type="text" />
+          <input type="text" className="rounded-md" />
         </div>
       </div>
-      <div>Jobs List</div>
+      <div className="m-4">
+        <JobList items={REMOTECOMPANIES} />
+      </div>
     </div>
   );
 }
